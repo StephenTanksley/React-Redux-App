f653e48743f2e19ccdfe443bad2bbac630a66492
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-10-24 20:18:41 -0500

    Connected store to App. Need to check to see that state is being populated into the app.

--- star-wars-tourism/src/App.js
+++ star-wars-tourism/src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from "react-redux"
+import { getItinerary } from './actions/index';
 import './App.css';
 
-function App() {
+function App(props) {
+console.log(props)
+
   return (
     <div className="App">
       <h1>Hi</h1>
+
+      <div className="button-container">
+        <button onClick={() => props.getItinerary()} >
+          Plan Your Getaway
+        </button>
+      </div>
+      <div className="card-container">
+        <h3>Something here.</h3>
+        {/*We'll want to have a component here that spits out information about the planet we're wanting to visit here. This will be the result of the API call. */}
+      </div>
+
     </div>
   );
 }
 
-export default App;
+
+function mapStateToProps(state) {
+  return {
+    planet: state.planet,
+    isLoading: state.isLoading,
+    error: state.error
+  }
+}
+
+
+const mapDispatchToProps = {
+  getItinerary
+}
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+  )(App);
 
 
 //creating a Star Wars tourism app. Han and Chewie are down on their luck, so they're setting up shop in the Mos Eisley spaceport cantina looking for passengers. This is an app they'll use to give potential passengers information about the planets they'll be going to. Information on the planet's size, climate, residents, etc.
