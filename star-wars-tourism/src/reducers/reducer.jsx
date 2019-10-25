00b35ffa4a9e3a31693fa52565511d8f5cf563fc
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-10-24 19:53:03 -0500

    Created a reducer for the cases of GET_DESTINATION, GOT_DESTINATION, GET_DESTINATION_FAILED.

--- star-wars-tourism/src/reducers/reducer.jsx
+++ star-wars-tourism/src/reducers/reducer.jsx
@@ -1,1 +1,38 @@
-import { GET_DESTINATION } from '../actions/index'
+import { 
+    GET_DESTINATION,
+    GOT_DESTINATION,
+    GET_DESTINATION_FAILED    
+    } from '../actions/index'
+
+const initialState = {
+    planet: null,
+    isLoading: false;
+    error: null
+}
+
+export function reducer(state=initialState, action) {
+    switch(action.type) {
+        case GET_DESTINATION:
+            return {
+            ...state,
+            isLoading: true;
+        }
+
+        case GOT_DESTINATION: 
+        return {
+            ...state,
+            planet: action.payload,
+            isLoading: false
+        }
+
+        case GET_DESTINATION_FAILED:
+            return {
+                ...state,
+                error: action.payload,
+                isLoading: false
+            }
+        
+            default: 
+            return state;
+    }
+}
